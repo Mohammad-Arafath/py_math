fddb7a0c1628f8c8c27615ba4af82937544129d6
Mohammad Arafath uz Zaman Khan <mzamankhan1@toromail.csudh.edu>
2025-01-30 12:35:31 -0800

    Update test_math_utils.py
    Few changes to check

--- test_math_utils.py
+++ test_math_utils.py
@@ -14,13 +14,13 @@ def math_utils():
     return MathUtils()
 
 def test_add(math_utils):
-    assert math_utils.add(3, 2) == 5
+    assert math_utils.add(3, 3) == 5
     assert math_utils.add(-3, -2) == -5
     assert math_utils.add(0, 5) == 5
     assert math_utils.add(5, -2) == 3
 
 def test_subtract(math_utils):
-    assert math_utils.subtract(10, 5) == 5
+    assert math_utils.subtract(10, 4) == 6
     assert math_utils.subtract(-5, -2) == -3
     assert math_utils.subtract(0, 7) == -7
     assert math_utils.subtract(3, 5) == -2
